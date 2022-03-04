4667f9aa76d2068397ede7c10d77e1e7d29a95b6
Ptitjen <97309340+Ptitjen@users.noreply.github.com>
2022-03-04 18:15:47 +0100

    Marche uniquement pour buffers plus grands que fichiers

---
+++ Get_next_line/get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeulliot <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/03 13:41:46 by jeulliot          #+#    #+#             */
+/*   Updated: 2022/03/03 13:41:49 by jeulliot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include<stdlib.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include<sys/stat.h>
+# include<fcntl.h>
+
+
+char	*get_next_line(int fd);
+char	*ft_strncpy(char *src, int index, int end);
+
+
+#endif
